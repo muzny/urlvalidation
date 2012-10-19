4c60203472f38955cd1705ad05de13f3ec67f200
Laura Dong <ledong@attu3.cs.washington.edu>
2012-10-19 13:03:05 -0700

    implemented mergesort() and threw in comments

--- sortUrls.py
+++ sortUrls.py
@@ -2,9 +2,31 @@
 
 import sys
 
+"""
+Comparison functions
+
+Arguments:
+    a - first element to compare
+    b - second element to compare
+Returns:
+    negative integer if the first precedes the second
+    0 if both arguments have equal ordering
+    positive integer if the first succeeds the second
+"""
+
 # default to sort by length
 def default_cmp(a, b):
     return len(a) - len(b)
+
+"""
+Sorting functions
+
+Arguments:
+    list - list of elements (url strings)
+    cmp  - comparison function
+Returns:
+    sorted list of elements
+"""
 
 def quicksort(list, cmp=default_cmp):
     # test
@@ -15,11 +37,39 @@ def insertionsort(list, cmp=default_cmp):
     pass
 
 def mergesort(list, cmp=default_cmp):
-    pass
+    size = len(list)
+    if (size <= 1):
+        return list
+    left = mergesort(list[0 : size/2], cmp)
+    right = mergesort(list[size/2 : size], cmp)
+    return merge(left, right, cmp)
+
+def merge(left, right, cmp):
+    result = []
+    while (len(left) > 0 or len(right) > 0):
+        # both non empty
+        if (len(left) > 0 and len(right) > 0):
+            c = cmp(left[0], right[0])
+            if (c <= 0):
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+        # right is empty
+        elif (len(left) > 0):
+            result.append(left.pop(0))
+        # left is empty
+        else:
+            result.append(right.pop(0))
+    return result
 
 def bucketsort(list, cmp=default_cmp):
     pass
 
+"""
+Main
+
+Parse command line arguments and execute sort functions.
+"""
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Incorrect usage. Please specify an input & output file.")
