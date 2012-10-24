77276c4a4f01c7a8f753daf5c06871278bb01784
Cullen Su <cullensu90@gmail.com>
2012-10-24 15:15:01 -0700

    Adding testing script and input test files.

---
+++ tests.bat
@@ -0,0 +1,18 @@
+python sortUrls.py tests/input_test_1.txt tests/output_test_1.txt
+python sortUrls.py tests/input_test_2.txt tests/output_test_2.txt
+python sortUrls.py tests/input_test_3.txt tests/output_test_3.txt
+python sortUrls.py tests/input_test_4.txt tests/output_test_4.txt
+
+python sortUrls.py tests/input_test_1-2.txt tests/output_test_1-2.txt
+python sortUrls.py tests/input_test_2-2.txt tests/output_test_2-2.txt
+python sortUrls.py tests/input_test_3-2.txt tests/output_test_3-2.txt
+python sortUrls.py tests/input_test_4-2.txt tests/output_test_4-2.txt
+
+python sortUrls.py tests/input_test_1-3.txt tests/output_test_1-3.txt
+python sortUrls.py tests/input_test_2-3.txt tests/output_test_2-3.txt
+python sortUrls.py tests/input_test_3-3.txt tests/output_test_3-3.txt
+python sortUrls.py tests/input_test_4-3.txt tests/output_test_4-3.txt
+
+python sortUrls.py tests/input_test_0.txt tests/output_test_0.txt
+python sortUrls.py tests/input_test_0-1.txt tests/output_test_0-1.txt
+python sortUrls.py tests/input_test_0-2.txt tests/output_test_0-2.txt
