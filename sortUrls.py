736bb93775d397f38e62e7392fc9ce3344076fca
Justin McManus <jmcmanu@gmail.com>
2012-11-07 16:54:25 -0800

    Removed option to specify sorting algorithm in input file. Added choice constraint to -s command line arg

--- sortUrls.py
+++ sortUrls.py
@@ -43,18 +43,18 @@ Parse command line arguments and execute sort functions.
 """
 if __name__ == "__main__":
     algos = {1: insertionsort, 2: mergesort, 3: quicksort, 4: bucketsort,
-             5: selectionsort_alphabetical, 6: radixsort_alphabetical, 
+             5: selectionsort_alphabetical, 6: radixsort_alphabetical,
              7: mergesort_alphabetical, 8: heapsort_alphabetical}
 
     parser = argparse.ArgumentParser(description="""Sorting Madness!
         Given an input file containing one url per line, prints the sorted list
-        of urls to the output file. The desired sorting algorithm can be supplied using
-        an integer, either as a command-line argument or as the first line of the
-        input file. If both methods are used, the command-line argument wins. If no
-        sorting algorithm is supplied, the default is quicksort. """)
+        of urls to the output file. The desired sorting algorithm can be supplied as
+        a command-line argument. The default is quicksort.""")
     parser.add_argument('-i', '--input', help='the input file', required=True)
     parser.add_argument('-o', '--output', help='the output file', required=True)
-    parser.add_argument('-s', '--sort', type=int, help='the sorting algorithm. \
+    parser.add_argument('-s', '--sort', type=int,
+                        choices=list(range(1,len(algos.keys()))),
+                        help='the sorting algorithm. \
                         Must be an integer. possible values are:\n' +
                         '\n'.join(['%d:%s' % (k, algos[k].__name__) for k in algos.keys()]) )
     args = parser.parse_args()
@@ -75,15 +75,7 @@ if __name__ == "__main__":
         sys.exit()
 
     sel = 3  # default selection is quicksort
-
-    try:
-        if len(urls) > 0:
-            sel = int(urls[0]) # try getting the sorting alg from the file
-            urls = urls[1:] # this line isn't executed if above failes
-    except ValueError:
-        pass
-
-    if args.sort is not None: # command line always wins
+    if args.sort is not None: # try getting sort selection from command-line args
         sel = args.sort
 
     sorter = algos[sel](urls)
